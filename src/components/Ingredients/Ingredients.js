feaa229a31ddfd89bf07f59f4e73074b9588f397
veja947 <veja947@gmail.com>
2021-04-30 23:32:36 -0400

    first implement 'adding ingredients into the form' feature

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from "./IngredientList";
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+
+    const [ingredientsArray, setIngredientsArray] = useState([]);
+
+    const addIngredientHandler = ingredient => {
+        console.log(ingredient);
+        setIngredientsArray(prevIngredients => [...prevIngredients, {
+            id: Math.random().toString(),
+            ...ingredient
+        }])
+    }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={ingredientsArray} onRemoveItem={() => {}} />
       </section>
     </div>
   );
